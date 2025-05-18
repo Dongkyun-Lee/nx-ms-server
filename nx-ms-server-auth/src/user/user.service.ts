928b0d17be38b3e322cd9958c94867cd26165d12
Dongkyun <leekyun0610@5works.co.kr>
2025-05-18 14:21:38 +0900

    feat: [auth] 유저 삭제요청 쿼리 수정

--- nx-ms-server-auth/src/user/user.service.ts
+++ nx-ms-server-auth/src/user/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './entity/user.entity';
-import { CreateUserRequestDto, CreateUserResponseDto, DeleteUserRequestDto, UpdateUserRequestDto, UpdateUserResponseDto } from './dto/user.dto';
+import { CreateUserRequestDto, CreateUserResponseDto, DeleteUserRequestDto, DeleteUserResponseDto, UpdateUserRequestDto, UpdateUserResponseDto } from './dto/user.dto';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
@@ -55,6 +55,6 @@ export class UserService {
     if (!deletedUser) {
       throw new NotFoundException(`User with EMAIL "${email}" not found`);
     }
-    return deletedUser;
+    return DeleteUserResponseDto.fromDocument(deletedUser);
   }
 }
