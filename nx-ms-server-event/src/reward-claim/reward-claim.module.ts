e176a89af69dbae47007e26bccccf35f6ad747f8
Dongkyun <leekyun0610@gmail.com>
2025-05-19 21:26:03 +0900

    fix: fix typo reward module

--- nx-ms-server-event/src/reward-claim/reward-claim.module.ts
+++ nx-ms-server-event/src/reward-claim/reward-claim.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RewardClaimController } from './reward-claim.controller';
 import { RewardClaimService } from './reward-claim.service';
-import { RewardService } from 'src/reward/reward.service';
 
 @Module({
   controllers: [RewardClaimController],
   providers: [RewardClaimService],
-  exports: [RewardService],
+  exports: [RewardClaimService],
 })
 export class RewardClaimModule {}
